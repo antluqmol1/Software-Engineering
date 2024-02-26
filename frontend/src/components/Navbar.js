fcc758ed969a277bd6c6425864fa2742e03f6618
Mort1J1 <98363767+Mort1J1@users.noreply.github.com>
2024-02-26 22:18:00 +0100

    Hope the node_modules will disappear now but IDK, added a simple nav bar with can navigate between four components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/create-user">Create User</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
