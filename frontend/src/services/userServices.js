04c76ebbc9d961c93495f63b444fa95a92dd5786
AdrianMoen <adrian.moen01@gmail.com>
2024-05-08 00:34:38 +0200

    added require method and authentication wraps for views

--- frontend/src/services/userServices.js
+++ frontend/src/services/userServices.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
 import config from '../config';
 
 
@@ -11,12 +11,47 @@ const createUser = async (first_name, last_name, username, email, password, csrf
             password,
           }, {
             headers: {
-              'X-CSRFToken': csrftoken,  // Include CSRF token in the headers
+              'X-CSRFToken': csrfToken,  // Include CSRF token in the headers
             }
     });
 }
 
+const getProfile = async () => {
+    return axios.get(`${config.API_BASE_URL}/user/profile/get`, {
+        withCredentials: true,
+    })
+}
+
+const updateProfile = async (field, updatedValue, csrfToken) => {
+    const response = await axios.put(
+        `${config.API_BASE_URL}/profile/update-profile/`,
+        {
+          field: field,
+          value: updatedValue,
+        },
+        {
+            headers: {
+              "X-CSRFToken": csrfToken,
+            },
+        }
+      );
+}
+
+const getPictureBase64 = async () => {
+    return axios.get(`${config.API_BASE_URL}/user/profile/get-picture/`, {
+        withCredentials: true,
+    });
+}
+
+const uploadPicture = async () => {
+
+}
+
+const getAllPictures = async () => {
+
+}
+
 
 export default {
-    createUser, 
+    createUser, getProfile, updateProfile, getPictureBase64, uploadPicture, getAllPictures,
   };
